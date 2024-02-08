ffafc9c6ddb7999ab135904f9cc5d6f16fc0634a
JACKERS11337 <158367573+JACKERS11337@users.noreply.github.com>
2024-02-08 21:10:54 +0300

    Revert "filter"

--- src/App.jsx
+++ src/App.jsx
@@ -1,16 +1,12 @@
 import { Header } from "./widgets/header";
 import { Product } from "./components/card";
-import { Filter } from "./components/filter";
 import cardsData from "../src/data/cards.json";
 
 function App() {
   return (
     <>
       <Header />
-      <div style={{ display: "flex" }}>
-        <Product product={cardsData[0]} />
-        <Filter />
-      </div>
+      <Product product={cardsData[0]} />
     </>
   );
 }
